8f5c21ea6da07be7a36db4bf1c44dc90fa8cd8a8
Guilherme Vitório <gui.vitoriot@gmail.com>
2025-03-31 09:50:29 -0300

    Enviando exercicio02

---
+++ exercicio02.py
@@ -0,0 +1,13 @@
+n1=int(input("Digite o número 1: "))
+n2=int(input("Digite o número 2: "))
+soma= (n1+n2)
+sub= (n1-n2)
+mult= (n1*n2)
+div= (n1/n2)
+print("Os números são: ",n1,n2)
+print(f"\n{n1}+{n2} = {soma}")
+print(f"{n1}-{n2}= {sub}")
+print(f"{n1}*{n2}= {mult}")
+print(f"{n1}/{n2}= {div}")
+
+
